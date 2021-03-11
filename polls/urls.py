dd0804e9e274d38f9dc9451bea59ccf0be45a912
Julian Merida <julianmr97@gmail.com>
2021-03-11 01:33:36 -0300

    Implement buy_item and product detail

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:item_id>/', views.detail, name='detail')
+    path('<int:item_id>/', views.detail, name='detail'),
+    path('<int:item_id>/buy', views.buy_item, name='buy')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Item
 
@@ -13,3 +14,16 @@ def index(request):
 def detail(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
     return render(request, 'polls/detail.html', {'item': item})
+
+
+def buy_item(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    if item.quantity > 0:
+        item.quantity = item.quantity - 1
+        item.save()
+    else:
+        return render(request, 'polls/detail.html', {
+            'item': item,
+            'error_message': "Product out of stock",
+            })
+    return HttpResponseRedirect(reverse('polls:detail', args=(item.id,)))
