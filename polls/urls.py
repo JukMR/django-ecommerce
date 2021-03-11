321f10ecfec49bc9fff071fbe5f7dfe7fd9d972c
Julian Merida <julianmr97@gmail.com>
2021-03-11 01:46:03 -0300

    Change Index and Detail views for django generics ones

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:item_id>/', views.detail, name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:item_id>/buy', views.buy_item, name='buy')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Item
 
 
-def index(request):
-    item_list = Item.objects.all()
-    context = {'item_list': item_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'item_list'
+
+    def get_queryset(self):
+        return Item.objects.all()
 
 
-def detail(request, item_id):
-    item = get_object_or_404(Item, pk=item_id)
-    return render(request, 'polls/detail.html', {'item': item})
+class DetailView(generic.DetailView):
+    model = Item
+    template_name = 'polls/detail.html'
 
 
 def buy_item(request, item_id):
